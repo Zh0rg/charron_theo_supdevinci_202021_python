cba225b0eef1c3ea8ac5b32057f3010648e2fd97
Charron Théo <2t0h0e2o@gmail.com>
2020-12-03 14:51:22 +0100

    Small corrections

--- exercice11.py
+++ exercice11.py
@@ -4,14 +4,14 @@ def binary_search(l, value):
     index = -1
 
     while start <= end:
-        middle = (end - start) // 2
+        middle = (end + start) // 2
 
         # Puisque la liste est triée, si l[middle] > valeur,
         # valeur se trouve entre debut et milieu
         if l[middle] > value:
             end = middle
         # Et vice-versa
-    elif l[middle] > value:
+        elif l[middle] < value:
             start = middle
         # Si l[middle] == valeur
         else:
@@ -22,5 +22,5 @@ def binary_search(l, value):
 
 #Test
 array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-print("L'élément 3 est à l'indice {}.".format(binary_search(array, 3)))
+elt = 6
+print("L'élément {} est à l'indice {}.".format(elt, binary_search(array, elt)))
